61702f920d260816e68bc1d4a2057dad999aacae
mvttn <85101143+mvttn@users.noreply.github.com>
2024-01-02 23:34:03 +1100

    Create input normaliser function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -6,3 +6,10 @@ function getComputerChoice() {
   return computerChoice;
 }
 
+function normaliseInput(input) {
+  if (input.length === 0) {
+    return;
+  }
+  return input[0].toUpperCase() + input.substring(1).toLowerCase();
+}
+
